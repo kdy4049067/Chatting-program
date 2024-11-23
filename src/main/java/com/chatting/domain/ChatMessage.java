53beb48b8cabaae22cd9a21caa017a1ab29e980a
daeyeon <kdy4410121@naver.com>
2024-11-23 19:36:02 +0900

    feat: data 어노테이션 , messageType enum 추가

--- src/main/java/com/chatting/domain/ChatMessage.java
+++ src/main/java/com/chatting/domain/ChatMessage.java
@@ -1,7 +1,15 @@
 package com.chatting.domain;
 
+import lombok.Data;
+
+@Data
 public class ChatMessage {
 
+    public enum MessageType{
+        ENTER, TALK, EXIT
+    }
+
+    private MessageType messageType;
     private String message;
     private String sender;
     private String roomId;
