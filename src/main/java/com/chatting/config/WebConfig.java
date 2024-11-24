c1d8b1ea125ea6afca7cb9bc75a4c8746aeb4b0d
daeyeon <kdy4410121@naver.com>
2024-11-24 18:16:23 +0900

    fix: 패키지 경로 변경

--- src/main/java/com/chatting/config/WebConfig.java
+++ src/main/java/com/chatting/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.config;
+package com.chatting.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
