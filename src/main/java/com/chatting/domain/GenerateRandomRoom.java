239d5f58279e1159a1365f1c7136ff70fc9ac75b
daeyeon <kdy4410121@naver.com>
2024-11-24 15:50:18 +0900

    feat: @Component 추가

--- src/main/java/com/chatting/domain/GenerateRandomRoom.java
+++ src/main/java/com/chatting/domain/GenerateRandomRoom.java
@@ -1,7 +1,10 @@
 package com.chatting.domain;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component
 public class GenerateRandomRoom implements GenerateRandom{
 
     @Override
