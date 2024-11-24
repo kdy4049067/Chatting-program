f671a55581ee098c55b5856f6e267bd38a2344ee
daeyeon <kdy4410121@naver.com>
2024-11-24 18:34:35 +0900

    feat: getMapping을 통한 채팅방 조회 함수

---
+++ src/main/java/com/chatting/controller/ChatRoomController.java
@@ -0,0 +1,40 @@
+package com.chatting.controller;
+
+import com.chatting.domain.ChatRoom;
+import com.chatting.service.ChatRoomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static javax.security.auth.callback.ConfirmationCallback.OK;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatRoomController {
+
+    private final ChatRoomService chatRoomService;
+
+    public ChatRoomController(ChatRoomService chatRoomService){
+        this.chatRoomService = chatRoomService;
+    }
+
+    @GetMapping("/rooms")
+    public ResponseEntity<List<ChatRoom>> room(){
+        List<ChatRoom> chatRoom = chatRoomService.findAllRoom();
+        return ResponseEntity.status(OK).body(chatRoom);
+    }
+
+    @PostMapping("/room")
+    public ResponseEntity<ChatRoom> createRoom(@RequestParam String name) {
+        ChatRoom chatRoom = chatRoomService.createRoom(name);
+        return ResponseEntity.status(OK).body(chatRoom);
+    }
+
+    @GetMapping("/room/{roomId}")
+    public ResponseEntity<ChatRoom> findRoom(@PathVariable String roomId){
+        ChatRoom chatRoom = chatRoomService.findRoomById(roomId);
+        return ResponseEntity.status(OK).body(chatRoom);
+    }
+
+}
