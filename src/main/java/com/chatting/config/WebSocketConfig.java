7adf19fe803a726f9cee5f847ad9b6648dbd2ee6
daeyeon <kdy4410121@naver.com>
2024-11-28 11:21:21 +0900

    fix: 공백 제거

--- src/main/java/com/chatting/config/WebSocketConfig.java
+++ src/main/java/com/chatting/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.config;
+package com.chatting.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -8,11 +8,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
-@EnableWebSocketMessageBroker //STOMP 프로토콜을 기반으로 한 메시지 브로커를 활성화, 클라이언트 사이에서 메시지를 라우팅하는 역할을 함.
-                              //webSocket 메시징을 간단하게 설정할 수 있음
-
+@EnableWebSocketMessageBroker //STOMP 프로토콜을 기반으로 한 메시지 브로커를 활성화, 클라이언트 사이에서 메시지를 라우팅하는 역할을 함, webSocket 메시징을 간단하게 설정할 수 있음
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
         config.enableSimpleBroker("/sub"); //sub로 시작하는 경로에 대한 메시지를 브로커가 처리하도록 설정, 메시지 브로커의 prefix 등록
