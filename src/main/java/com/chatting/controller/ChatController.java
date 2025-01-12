88c714e7733e05939fb7d5b81f5b750388ad3794
daeyeon <kdy4410121@naver.com>
2025-01-12 15:18:38 +0900

    feat: 채팅 내역 저장하는 postMapping 생성

--- src/main/java/com/chatting/controller/ChatController.java
+++ src/main/java/com/chatting/controller/ChatController.java
@@ -4,28 +4,44 @@ import com.chatting.ChattingApplication;
 import com.chatting.Repository.ChatRoomRepository;
 import com.chatting.domain.ChatMessage;
 import com.chatting.domain.ChatRoom;
+import com.chatting.dto.ChatDto;
+import com.chatting.service.ChatRoomService;
+import com.chatting.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ChatController {
 
     private final SimpMessageSendingOperations messagingTemplate;
-    private final ChatRoomRepository chatRoomRepository;
+    private final ChatService chatService;
+    private final ChatRoomService chatRoomService;
 
-    public ChatController(SimpMessageSendingOperations messagingTemplate, ChatRoomRepository chatRoomRepository){
+    public ChatController(SimpMessageSendingOperations messagingTemplate, ChatService chatService, ChatRoomService chatRoomService){
         this.messagingTemplate = messagingTemplate;
-        this.chatRoomRepository = chatRoomRepository;
+        this.chatService = chatService;
+        this.chatRoomService = chatRoomService;
     }
 
    @MessageMapping("/chat/send")
-   public void sendMessage(ChatMessage chatMessage){
-        messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(), chatMessage);
+   public void sendMessage(ChatDto chatDto){
+        messagingTemplate.convertAndSend("/sub/chat/room/" + chatDto.roomId(), chatDto);
+   }
+
+   @PostMapping("/chat/save")
+    public ResponseEntity<ChatDto> saveMessage(@RequestBody ChatDto chatDto){
+        ChatRoom chatRoom = chatRoomService.findByRoomId(chatDto.roomId());
+        ChatDto chatDto1 = chatService.createChat(chatDto.messageType(), chatDto.message(), chatDto.sender(), chatRoom);
+
+        return ResponseEntity.ok(chatDto1);
    }
 
 }
