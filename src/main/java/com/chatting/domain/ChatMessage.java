354ca293a154b39452eb705d5bafc046ee49b4af
daeyeon <kdy4410121@naver.com>
2025-01-12 15:18:52 +0900

    feat: 채팅 도메인 생성

--- src/main/java/com/chatting/domain/ChatMessage.java
+++ src/main/java/com/chatting/domain/ChatMessage.java
@@ -1,17 +1,41 @@
 package com.chatting.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "ChatMessage")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ChatMessage {
+
+    public ChatMessage(MessageType messageType, String message, String sender, ChatRoom chatRoom){
+        this.messageType = messageType;
+        this.message = message;
+        this.sender = sender;
+        this.chatRoom = chatRoom;
+    }
 
     public enum MessageType{
         ENTER, TALK, EXIT
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int messageId;
+    @Column
     private MessageType messageType;
+    @Column
     private String message;
+    @Column
     private String sender;
-    private String roomId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roomId")
+    private ChatRoom chatRoom;
 
 }
